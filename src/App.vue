57d23441e34427a540ef7c8fecfd107dde12075d
Guilherme <guilhermefrance9@gmail.com>
2025-09-18 16:56:52 -0300

    scoreboard

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,93 @@
-<script setup></script>
+
 
 <template>
+  <ScoreBoard/>
+<template v-if="this.question">
   <div>
+
     <h1 v-html="this.question"></h1>
 
-    <input type="radio" name="options" value="True" />
-    <label>True</label><br />
+     <template v-for="(answer, index) in this.answers" :key= "index">
+      <input 
+      :disabled="this.answerSubmitted"
+      type="radio" 
+      name="options" 
+      :value="answer"
+      v-model="this.chosen_answer"/>
+      <label v-html="answer"></label><br/>
+      </template>
+   
+    <button v-if="!this.answerSubmitted" @click="this.submitAnswer()" :disabled="this.answerSubmitted" class="send" type="button">Enviar resposta</button>
 
-    <input type="radio" name="options" value="False" />
-    <label>False</label><br />
-
-    <button class="send" type="button">Send</button>
-  </div>
+    <section v-if="this.answerSubmitted">
+      <h4 v-if="this.chosen_answer == this.CorrectAnswer" >&#128640; Parabéns, certa resposta!</h4>
+      <h4 v-else v-html="'&#10060; Desculpe, você escolheu a resposta errada! A correta é ' + this.CorrectAnswer "</h4>
+      <button v-if="this.answerSubmitted" @click="getNewQuestion()" class="send" type="button">Próxima pergunta</button>
+    </section>
+  </div>  
+  
+</template>
 </template>
 
 <script>
+
+import ScoreBoard from './components/ScoreBoard.vue'
+
 export default {
   name: 'App',
+  components: {
+    ScoreBoard
+  },
 
   data() {
     return {
       question: undefined,
       IncorrectAnswer: undefined,
       CorrectAnswer: undefined,
+      chosen_answer: undefined,
+      answerSubmitted: false
     }
   },
 computed: {
   answers(){
   var answers = JSON.parse(JSON.stringify(this.IncorrectAnswer));
-  answers.splice( Math.round(Math.random()*4 ), 0, this.CorrectAnswer)
+  answers.splice( Math.round(Math.random()* 4 ), 0, this.CorrectAnswer)
+  console.log(answers)
   return answers;
   }
 },
-  created() {
+methods: {
+  submitAnswer(){
+    if (!this.chosen_answer){
+      alert('Escolha uma das alternativas!')
+    }else {
+      this.answerSubmitted = true;
+      if(this.chosen_answer == this.CorrectAnswer){
+        alert('Você acertou!')
+      }else{
+        alert('Você errou! :(')
+      }
+    }
+  },
+
+  getNewQuestion() {
+
+    this.answerSubmitted = false;
+    this.chosen_answer = undefined;
+    
+
     this.axios
-      .get('https://opentdb.com/api.php?amount=1&category=15&difficulty=medium')
+      .get('https://opentdb.com/api.php?amount=1&category=18&difficulty=easy')
       .then((response) => {
         this.question = response.data.results[0].question;
         this.IncorrectAnswer = response.data.results[0].incorrect_answers;
         this.CorrectAnswer = response.data.results[0].correct_answer;
         console.log(response.data.results[0])
       })
+  }
+},
+  created() {
+    this.getNewQuestion()
   },
 }
 
---
+++ src/components/ScoreBoard.vue
@@ -0,0 +1,11 @@
+<template>
+    <h2>Player <span>0</span> x <span>0</span> Computer</h2>
+</template>
+
+<script>
+
+</script>
+
+<style lang="sass">
+
+</style>
